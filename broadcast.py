453a17484baa837ee8912193e0aa909b843f39cc
Naren Niranjan <narenn@umich.edu>
2017-05-02 00:47:21 -0400

    Added nick change and ACTION behavior, as well as a prefix for seeing which chat the message comes from. With this the bot is at feature parity with narenniranjan/mumble2irc

--- broadcast.py
+++ broadcast.py
@@ -5,11 +5,30 @@ import tornado.platform.asyncio
 import asyncio
 import argparse
 import json
+import re
+import bs4
 from enum import Enum
 
 
 def escape_nick(nick):
     return nick.replace("", "\u00AD")
+
+
+def strip_html(html):
+    return bs4.BeautifulSoup(html).text
+
+
+def linkify(url):
+    # Replace url to link
+    urls = re.compile(
+            r"((https?):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)",
+            re.MULTILINE | re.UNICODE)
+    url = urls.sub(r'<a href="\1" target="_blank">\1</a>', url)
+    # Replace email to mailto
+    urls = re.compile(
+            r"([\w\-\.]+@(\w[\w\-]+\.)+[\w\-]+)", re.MULTILINE | re.UNICODE)
+    url = urls.sub(r'<a href="mailto:\1">\1</a>', url)
+    return url
 
 
 class MsgType(Enum):
@@ -36,8 +55,9 @@ class Bus(object):
 
 
 class IRCClient(pydle.Client):
-    def __init__(self, bus, nick, channel):
+    def __init__(self, bus, nick, channel, prefix):
         self.bus = bus
+        self.prefix = prefix
         self.channel = channel
         self.bus.add_listener(self.on_bus_message)
         super().__init__(nick)
@@ -54,6 +74,12 @@ class IRCClient(pydle.Client):
     def on_quit(self, user, reason):
         self.bus.broadcast(self, user, self.channel, MsgType.PART)
 
+    def irc_action(self, origin, action):
+        self.bus.broadcast(self, origin, action, MsgType.ACTION)
+
+    def on_nick_change(self, old, new):
+        self.bus.broadcast(self, old, new, MsgType.NICK)
+
     def on_message(self, source, target, message):
         self.bus.broadcast(self, target, message, MsgType.TEXT)
 
@@ -63,25 +89,36 @@ class IRCClient(pydle.Client):
         author = escape_nick(author)
         if msg_type == MsgType.TEXT:
             self.message(
-                self.channel, "\x02{}:\x02 {}".format(author, message))
+                self.channel, "[{}] \x02{}:\x02 {}".format(
+                                                        source.prefix,
+                                                        author, message))
         elif msg_type == MsgType.ACTION:
             self.message(
-                self.channel, "*{} {}*".format(author, message))
+                self.channel, "[{}] *{} {}*".format(
+                                                source.prefix,
+                                                author, message))
         elif msg_type == MsgType.NICK:
             self.message(
-                self.channel, "{} is now known as {}".format(author, message))
+                self.channel, "[{}] {} is now known as {}".format(
+                                                            source.prefix,
+                                                            author, message))
         elif msg_type == MsgType.JOIN:
             self.message(
-                self.channel, ">>> {} has joined {}".format(author, message))
+                self.channel, "[{}] >>> {} has joined {}".format(
+                                                            source.prefix,
+                                                            author, message))
         elif msg_type == MsgType.PART:
             self.message(
-                self.channel, "<<< {} has left {}".format(author, message))
+                self.channel, "[{}] <<< {} has left {}".format(
+                                                            source.prefix,
+                                                            author, message))
 
 
 class MumbleClient(mumble.Client):
-    def __init__(self, bus, channel_id):
+    def __init__(self, bus, channel_id, prefix):
         self.bus = bus
         self.channel_id = channel_id
+        self.prefix = prefix
         self.bus.add_listener(self.on_bus_message)
         super().__init__()
 
@@ -99,7 +136,8 @@ class MumbleClient(mumble.Client):
                         self.me.get_channel().name, MsgType.JOIN)
 
     def text_message_received(self, origin, target, message):
-        self.bus.broadcast(self, origin.name, message, MsgType.TEXT)
+        self.bus.broadcast(
+                        self, origin.name, strip_html(message), MsgType.TEXT)
 
     def on_bus_message(self, source, author, message, msg_type):
         if self == source:
@@ -107,23 +145,32 @@ class MumbleClient(mumble.Client):
         if msg_type == MsgType.TEXT:
             self.send_text_message(
                     self.channels[self.channel_id],
-                    "<b>{}:</b> {}".format(author, message))
+                    "[{}] <b>{}:</b> {}".format(
+                                            source.prefix,
+                                            author, linkify(message)))
         elif msg_type == MsgType.ACTION:
             self.send_text_message(
                     self.channels[self.channel_id],
-                    "<i>{} {}</i>".format(author, message))
+                    "[{}] <i>{} {}</i>".format(source.prefix, author, message))
+
         elif msg_type == MsgType.NICK:
             self.send_text_message(
                     self.channels[self.channel_id],
-                    "<i>{} is now known as {}</i>".format(author, message))
+                    "[{}] <i>{} is now known as {}</i>".format(
+                                                            source.prefix,
+                                                            author, message))
         elif msg_type == MsgType.JOIN:
             self.send_text_message(
                     self.channels[self.channel_id],
-                    "{} has joined {}".format(author, message))
+                    "[{}] {} has joined {}".format(
+                                                source.prefix,
+                                                author, message))
         elif msg_type == MsgType.PART:
             self.send_text_message(
                     self.channels[self.channel_id],
-                    "{} has left {}".format(author, message))
+                    "[{}] {} has left {}".format(
+                                                source.prefix,
+                                                author, message))
 
 if __name__ == "__main__":
     running_services = []
@@ -148,14 +195,18 @@ if __name__ == "__main__":
 
     for service in config:
         if service['type'] == 'irc':
-            irc_client = IRCClient(bus, service['nick'], service['channel'])
+            irc_client = IRCClient(
+                                bus, service['nick'],
+                                service['channel'], service['prefix'])
             irc_client.connect(
                         service['server'], service['port'],
                         tls=bool(service['tls']),
                         tls_verify=bool(service['tls_verify']))
             running_services.append(irc_client)
         if service['type'] == 'mumble':
-            mumble_client = MumbleClient(bus, service['channel_id'])
+            mumble_client = MumbleClient(
+                                    bus, service['channel_id'],
+                                    service['prefix'])
             ssl_ctx = ssl.create_default_context()
             ssl_ctx.check_hostname = False
             ssl_ctx.verify_mode = ssl.CERT_NONE
